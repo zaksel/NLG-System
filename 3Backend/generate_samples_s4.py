6d76f0128806662d3171bfd1643b4d4c8e32438d
derEskimo <Atze2000!>
2019-09-22 11:52:11 +0200

    Implement Strategy BERT-GPT2 Hybrid

--- 3Backend/generate_samples_s4.py
+++ 3Backend/generate_samples_s4.py
@@ -1,60 +1,30 @@
-#Strategie 4 Bert-GPT-2 Hybrid
+#Strategie 4 Bert-GPT2 Hybrid
 import json
 import os
 import numpy as np
 import tensorflow as tf
 import torch
-from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
-import googletrans
-import time
+from pytorch_pretrained_bert import BertTokenizer, BertForMaskedLM
 
 import sys
 sys.path.insert(0, '.\src')
 import model, sample, encoder
 
-from tensorflow.python import debug as tf_debug
-
 
 class Model(object):
-    """
-    Start session
-    :model_name=117M : String, which model to use
-    :seed=None : Integer seed for random number generators, fix seed to reproduce
-     results
-    :nsamples=1 : Number of samples to return total
-    :batch_size=1 : Number of batches (only affects speed/memory).  Must divide nsamples.
-    :length=None : Number of tokens in generated text, if None (default), is
-     determined by model hyperparameters
-    :temperature=1 : Float value controlling randomness in boltzmann
-     distribution. Lower temperature results in less random completions. As the
-     temperature approaches zero, the model will become deterministic and
-     repetitive. Higher temperature results in more random completions.
-    :top_k=0 : Integer value controlling diversity. 1 means only 1 word is
-     considered for each step (token), resulting in deterministic completions,
-     while 40 means 40 words are considered at each step. 0 (default) is a
-     special setting meaning no restrictions. 40 generally is a good value.
-    :top_p=0.0 : Float value controlling diversity. Implements nucleus sampling,
-     overriding top_k if set to a value > 0. A good setting is 0.9.
-    """
-    def __init__(self,
-                 model_name='ISW_Model',
-                 seed=0,
-                 length=3,
-                 temperature=1,
-                 top_k=20, top_p=0,
-                 lang_target='de'):
+    def __init__(self, **kwargs):
+        model_name = kwargs['model']
+        seed = kwargs['seed']
+        self.length = kwargs['len']
+        top_k = kwargs['top_k']
 
         self.enc = encoder.get_encoder(model_name)
-        self.translator = googletrans.Translator()
         hparams = model.default_hparams()
         with open(os.path.join('./data/models', model_name, 'hparams.json')) as f:
             hparams.override_from_dict(json.load(f))
-        self.lang_model = hparams.n_lang
-        self.lang_target = lang_target
-        self.length = length
 
+        # start session
         self.sess = tf.Session()
-
         self.context = tf.placeholder(tf.int32, [1, None])
         np.random.seed(seed)
         tf.set_random_seed(seed)
@@ -62,8 +32,8 @@ class Model(object):
             hparams=hparams, length=self.length,
             context=self.context,
             batch_size=1,
-            temperature=temperature,
-            top_k=top_k, top_p=top_p
+            temperature=1,
+            top_k=top_k, top_p=0
         )
 
         # restore transformer model from last checkpoint
@@ -100,7 +70,7 @@ class Model(object):
                     tokenized_text[i] = predicted_token[0]
 
             text = ' '.join(tokenized_text)
-            text = text.replace(' ##', '').replace(' ,', ',').replace(' . ', ' ')
+            text = text.replace(' ##', '').replace(' ,', ',').replace(' . ', ' ') # TODO: decoding destroys grammar like interpunctuation
             print(text)
             return text
 
@@ -124,16 +94,4 @@ class Model(object):
         output = self.enc.decode(out[0])
         output = ".".join(output.split(".")[:-1]) + "."  # take only full sentences
 
-
-        #output = ". ".join(output.split(".")[:-1])+"." #take only full sentences
         return output
-
-if __name__ == '__main__':
-    model = Model()
-    start = time.time()
-    #print(model.generate(["Push the button", "right", "activate", "camera", "pictures", "family", "animals."]))    #a
-    #print(model.generate(["The C625AF camera", "five flash modes", "red-eye", "lens", "protected", "UV Filter.", "auto focus", "programmed shutter", "camera case"]))   #b
-    #print(model.generate(["If the viewfinder", "not sharp", "check", "eyepiece diopter adjustment", "knob", "near to the eyepiece.", "On-Off control", "button", "handgrip on the right", "thumb", "one press", "recording", "second", "stop."]))  #c
-    print(model.generate(["Im Jahre 1953", "MIT", "numerisch gesteuerte Werkzeugmaschine", "NC-Steuerungen", "Koordinatenbewegungen", "Produktionsmaschinen", "alphanumerische Informationen", "automatisch", "Informationen", "digitaler Form", "früher", "Lochstreifen", "heute", "Rechnerspeichermedien", "Server", "Nc-Steuerung", "decodiert", "Verfahrbewegungen", "Interpolation", "feine Schritte", "Sollwertsignale", "Bewegung der Achsen"]))
-
-    print(time.time()-start)
